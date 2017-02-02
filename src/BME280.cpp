51fbc8b5be84d575fe48b55c581b6dfe8d05ca8d
Daisuke Takahashi <daisuke@extendwings.com>
2017-02-02 17:13:39 +0900

    Add initial data

---
+++ src/BME280.cpp
@@ -0,0 +1,177 @@
+#include<BME280.hpp>
+
+#include "Arduino.h"
+#include<Wire.h>
+
+
+BME280::BME280() {
+    
+}
+
+/**
+ * High Level API
+ */
+bool BME280::begin(uint8_t address) {
+    return begin(address, REG_CONFIG_TSB_1000, REG_CONFIG_FILTER_OFF,
+          REG_CTRL_OSRS_1X, REG_CTRL_OSRS_1X, REG_CTRL_OSRS_1X,
+          REG_CTRL_MODE_NORMAL);
+}
+
+bool BME280::begin(uint8_t address, uint8_t t_sb, uint8_t filter,
+                   uint8_t osrs_t, uint8_t osrs_p, uint8_t osrs_h,
+                   uint8_t mode) {
+    return begin(address, t_sb, filter, 0x0, osrs_t, osrs_p, osrs_h, mode);
+}
+
+bool BME280::begin(uint8_t address, uint8_t t_sb, uint8_t filter,
+                   uint8_t spi3w_en, uint8_t osrs_t, uint8_t osrs_p,
+                   uint8_t osrs_h, uint8_t mode) {
+    Wire.begin();
+
+    uint8_t ctrl_meas = (osrs_t << 5) | (osrs_p << 2) | mode;
+    uint8_t config    = (t_sb << 5) | (filter << 2) | spi3w_en;
+    uint8_t ctrl_hum  = osrs_h;
+
+    setRegister(REG_ADDR_CTRL_HUM,  ctrl_hum);
+    setRegister(REG_ADDR_CTRL_MEAS, ctrl_meas);
+    setRegister(REG_ADDR_CONFIG,    config);
+
+    getCalibs();
+}
+
+void BME280::getData(float* temperature, float* pressure, float* humidity) {
+    int32_t adc_T, adc_P, adc_H;
+
+    getRawData(&adc_T, &adc_P, &adc_H);
+
+    _t_fine = calc_T_fine(adc_T);
+
+    *temperature = compensate_T(adc_T) / 100.0;
+    *pressure    = compensate_P(adc_P) / 256.0 / 100.0;
+    *humidity    = compensate_H(adc_H) / 1024.0;
+}
+
+
+/**
+ * Middle Level API
+ */
+void BME280::getCalibs() {
+    uint8_t data[32];
+
+    getRegister(REG_ADDR_CALIB[0],  data,    24);
+    getRegister(REG_ADDR_CALIB[24], data+24, 1);
+    getRegister(REG_ADDR_CALIB[25], data+25, 7);
+
+    _calib.dig_T1 = (data[1] << 8) | data[0];
+    _calib.dig_T2 = (data[3] << 8) | data[2];
+    _calib.dig_T3 = (data[5] << 8) | data[4];
+
+    _calib.dig_P1 = (data[7]  << 8) | data[6];
+    _calib.dig_P2 = (data[9]  << 8) | data[8];
+    _calib.dig_P3 = (data[11] << 8) | data[10];
+    _calib.dig_P4 = (data[13] << 8) | data[12];
+    _calib.dig_P5 = (data[15] << 8) | data[14];
+    _calib.dig_P6 = (data[17] << 8) | data[16];
+    _calib.dig_P7 = (data[19] << 8) | data[18];
+    _calib.dig_P8 = (data[21] << 8) | data[20];
+    _calib.dig_P9 = (data[23] << 8) | data[22];
+
+    _calib.dig_H1 = data[24];
+    _calib.dig_H2 = (data[26] << 8) | data[25];
+    _calib.dig_H3 = data[27];
+    _calib.dig_H4 = (0x0F & data[29]) | (data[28] << 4);
+    _calib.dig_H5 = ((data[29] >> 4) & 0x0F) | (data[30] << 4);
+    _calib.dig_H6 = data[31];
+}
+
+void BME280::getRawData(int32_t* temperature, int32_t pressure, int32_t humidity) {
+    uint32_t data[8];
+
+    getRegister(0xF7, data, 8);
+
+    *pressure    = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
+    *temperature = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+    *humidity    = (data[6] << 8) | data[7];
+}
+
+/* Compensate Methods are originally provided by BOSCH Sensortec */
+
+int32_t BME280::calc_T_fine(int32_t adc_T) {
+    int32_t var1, var2;
+
+    var1 = ((((adc_T >> 3) - ((int32_t)_calib.dig_T1 << 1))) * ((int32_t)_calib.dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((int32_t)_calib.dig_T1)) * ((adc_T >> 4) - ((int32_t)_calib.dig_T1))) >> 12) * ((int32_t)_calib.dig_T3)) >> 14;
+    return var1 + var2;
+}
+
+// Returns temperature in DegC, resolution is 0.01 DegC. Output value of “5123” equals 51.23 DegC.
+// t_fine carries fine temperature as global value
+int32_t BME280::compensate_T(int32_t adc_T) {
+    return (_t_fine * 5 + 128) >> 8;
+}
+
+// Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8 fractional bits).
+// Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+uint32_t BME280::compensate_P(int32_t adc_P) {
+    int64_t var1, var2, p;
+
+    var1 = ((int64_t)_t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)_calib.dig_P6;
+    var2 = var2 + ((var1 * (int64_t)_calib.dig_P5) << 17);
+    var2 = var2 + (((int64_t)_calib.dig_P4) << 35);
+    var1 = ((var1 * var1 * (int64_t)_calib.dig_P3) >> 8) + ((var1 * (int64_t)_calib.dig_P2) << 12);
+    var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)_calib.dig_P1) >> 33;
+    if (var1 == 0) {
+        return 0; // avoid exception caused by division by zero
+    }
+    p = 1048576 - adc_P;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = (((int64_t)_calib.dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = (((int64_t)_calib.dig_P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)_calib.dig_P7) << 4);
+
+    return (uint32_t)p;
+}
+
+// Returns humidity in %RH as unsigned 32 bit integer in Q22.10 format (22 integer and 10 fractional bits).
+// Output value of “47445” represents 47445/1024 = 46.333 %RH
+uint32_t BME280::compensate_H(int32_t adc_H) {
+    int32_t v_x1_u32r;
+
+    v_x1_u32r = (_t_fine - ((int32_t)76800));
+    v_x1_u32r = (((((adc_H << 14) - (((int32_t)_calib.dig_H4) << 20) - (((int32_t)_calib.dig_H5) * v_x1_u32r)) +
+                 ((int32_t)16384)) >> 15) * (((((((v_x1_u32r * ((int32_t)_calib.dig_H6)) >> 10) * (((v_x1_u32r *
+                     ((int32_t)_calib.dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) *
+                     ((int32_t)_calib.dig_H2) + 8192) >> 14));
+    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((int32_t)_calib.dig_H1)) >> 4));
+    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
+    v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
+
+    return (uint32_t)(v_x1_u32r >> 12);
+}
+
+
+/**
+ * Low Level API
+ */
+void BME280::setRegister(uint8_t reg, uint8_t byte) {
+    Wire.beginTransmission(_address);
+    Wire.write(reg);
+    Wire.write(byte);
+    Wire.endTransmission();
+}
+
+void BME280::getRegister(uint8_t reg, uint8_t* bytes, size_t bytes_len) {
+    Wire.beginTransmission(_address);
+    Wire.write(reg);
+    Wire.endTransmission();
+    Wire.requestFrom(_address, bytes_len);
+
+    for(uint8_t i = 0; i < bytes_len; i++) {
+        while(!Wire.available());
+
+        bytes[i] = Wire.read();
+    }
+}
+
+BME280 bme = BME280();
---
+++ src/BME280.hpp
@@ -0,0 +1,129 @@
+#ifndef BME280_h
+#define BME280_h
+
+#include "Arduino.h"
+
+typedef struct {
+	uint16_t dig_T1;
+	int16_t  dig_T2;
+	int16_t  dig_T3;
+
+	uint16_t dig_P1;
+	int16_t  dig_P2;
+	int16_t  dig_P3;
+	int16_t  dig_P4;
+	int16_t  dig_P5;
+	int16_t  dig_P6;
+	int16_t  dig_P7;
+	int16_t  dig_P8;
+	int16_t  dig_P9;
+
+	uint8_t  dig_H1;
+	int16_t  dig_H2;
+	uint8_t  dig_H3;
+	int16_t  dig_H4;
+	int16_t  dig_H5;
+	int8_t   dig_H6;
+} BME280_calib;
+
+class BME280 {
+	public:
+		BME280();
+
+		// SoftSerial in the future...
+		// BME280(uint8_t rxPin, uint8_t txPin);
+
+		// This library doesn't support SPI at the moment.
+		// TODO: Add support for SPI.
+		bool begin(uint8_t address);
+
+		bool begin(uint8_t address, uint8_t t_sb, uint8_t filter,
+		           uint8_t osrs_t, uint8_t osrs_p, uint8_t osrs_h,
+		           uint8_t mode);
+		           
+		bool begin(uint8_t address, uint8_t t_sb, uint8_t filter,
+		           uint8_t spi3w_en, uint8_t osrs_t, uint8_t osrs_p,
+		           uint8_t osrs_h, uint8_t mode);
+
+		void getData(float* temperature, float* pressure, float* humidity);
+
+		void setRegister(uint8_t reg, uint8_t byte);
+
+		void end();
+
+	private:
+		/**
+		 * Register Addresses
+		 */
+		const uint8_t REG_ADDR_CALIB[32] = {
+			0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91,
+			0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B,
+			0x9C, 0x9D, 0x9E, 0x9F, 0xA1, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7
+		};
+
+		const uint8_t REG_ADDR_CTRL_HUM  = 0xF2;
+		const uint8_t REG_ADDR_CTRL_MEAS = 0xF4;
+		const uint8_t REG_ADDR_CONFIG    = 0xF5;
+
+		/**
+		 * Register 0xF5 "config"
+		 */
+		// Controls t_standby, inactive duration. (Unit: ms)
+		const uint8_t REG_CONFIG_TSB_0_5  = 0x00;
+		const uint8_t REG_CONFIG_TSB_62_5 = 0x01;
+		const uint8_t REG_CONFIG_TSB_125  = 0x02;
+		const uint8_t REG_CONFIG_TSB_250  = 0x03;
+		const uint8_t REG_CONFIG_TSB_500  = 0x04;
+		const uint8_t REG_CONFIG_TSB_1000 = 0x05;
+		const uint8_t REG_CONFIG_TSB_10   = 0x06;
+		const uint8_t REG_CONFIG_TSB_20   = 0x07;
+
+		// Controls the time constant of the IIR filter.
+		const uint8_t REG_CONFIG_FILTER_OFF = 0x00;
+		const uint8_t REG_CONFIG_FILTER_2   = 0x01;
+		const uint8_t REG_CONFIG_FILTER_4   = 0x02;
+		const uint8_t REG_CONFIG_FILTER_8   = 0x03;
+		const uint8_t REG_CONFIG_FILTER_16  = 0x04;
+
+		/**
+		 * Register 0xF2 "ctrl_hum", 0xF4 "ctrl_meas"
+		 */
+		// Controls oversampling of data.
+		const uint8_t REG_CTRL_OSRS_SKIP = 0x00;
+		const uint8_t REG_CTRL_OSRS_1X   = 0x01;
+		const uint8_t REG_CTRL_OSRS_2X   = 0x02;
+		const uint8_t REG_CTRL_OSRS_4X   = 0x03;
+		const uint8_t REG_CTRL_OSRS_8X   = 0x04;
+		const uint8_t REG_CTRL_OSRS_16X  = 0x05;
+
+		// Controls the sensor mode of the device.
+		const uint8_t REG_CTRL_MODE_SLEEP  = 0x00;
+		const uint8_t REG_CTRL_MODE_FORCED = 0x01;
+		const uint8_t REG_CTRL_MODE_NORMAL = 0x03;
+
+
+		// SoftSerial in the future...
+		// uint8_t _rx_pin;
+		// uint8_t _tx_pin;
+
+		uint8_t _address;
+		uint8_t _chip_id;
+
+		BME280_calib _calib;
+		int32_t _t_fine;
+
+		void getCalibs();
+		void getRawData(int32_t* temperature, int32_t pressure, int32_t humidity);
+
+		int32_t calc_T_fine(int32_t adc_T);
+
+		int32_t compensate_T(int32_t adc_T);
+		uint32_t compensate_P(int32_t adc_P);
+		uint32_t compensate_H(int32_t adc_H);
+
+		void getRegister(uint8_t reg, uint8_t[] bytes, size_t bytes_len)
+}
+
+extern BME280 bme;
+
+#endif
